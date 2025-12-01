4608a23052b8645450ac3b4286faf7bad0156120
Emanuel <emanuelbrit16@gmail.com>
2025-12-01 16:40:13 -0300

    Creating Splash Screen

---
+++ app/index.tsx
@@ -0,0 +1,49 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+const Screen = () => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.logoArea}>
+                <Image 
+                    style={styles.logo}
+                    source={require('../assets/logo.png')}
+                />
+            </View>
+            <View style={styles.logoNameArea}>
+                <Text style={styles.logoName}>PizzaNotes</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F47C7C'
+    },
+    logoArea: {
+        width: 200,
+        height: 200,
+        backgroundColor: '#FFF',
+        borderRadius: '50%'
+    },
+    logo: {
+        width: 150,
+        height: 150
+    },
+    logoNameArea: {
+        paddingVertical: 5,
+        paddingHorizontal: 30,
+        marginTop: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 20
+    },
+    logoName: {
+        color: '#F47C7C',
+        fontSize: 25,
+        fontWeight: 'bold'
+    }
+})
+
+export default Screen
